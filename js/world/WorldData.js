7578ab6fa07ac45499020b0ecee6a542e17dc155
Tobias Küttner <t.kuettner@ee-mobility.com>
2018-02-24 17:31:44 +0100

    improved island generation

--- js/world/WorldData.js
+++ js/world/WorldData.js
@@ -9,7 +9,7 @@ class WorldData {
 		this.simplex = new SimplexNoise();
 
 
-		this.chunkSize = {x: 16, y: 64, z: 16};
+		this.chunkSize = {x: 16, y: 96, z: 16};
 		this.numberOfChunks = {x: 16, z: 16};
 		this.worldSize = {x: this.chunkSize.x * this.numberOfChunks.x,
 						  y: this.chunkSize.y,
@@ -32,7 +32,7 @@ class WorldData {
 				for(let y = 0; y < this.chunkSize.y; y++) {
 					if(y <= height - 2) {
 						this.cubeTypes[cubeIndex] = 0;
-					} else if(y <= height && y <= this.WATER_SURFACE_HEIGHT) {
+					} else if(y <= height && y <= this.getSandHeightFromSimplex(x, z)) {
 						this.cubeTypes[cubeIndex] = 2;
 					} else if(y <= height && y > this.WATER_SURFACE_HEIGHT) {
 						this.cubeTypes[cubeIndex] = 1;
@@ -49,31 +49,52 @@ class WorldData {
 
 	getHeightFromSimplex(x, z) {
 		let maxDistance = (this.chunkSize.x * this.numberOfChunks.x * 0.5 * Math.sqrt(2));
-		let normFactor = this.chunkSize.y / maxDistance * 0.75;
 		let x_dist = x - (this.chunkSize.x * this.numberOfChunks.x) / 2;
 		let z_dist = z - (this.chunkSize.z * this.numberOfChunks.z) / 2;
 
 		let distanceFromCenter = Math.sqrt(x_dist * x_dist + z_dist * z_dist);
 		if(distanceFromCenter === 0)
 			distanceFromCenter = 1;
-		distanceFromCenter *= 2;
 
-		// Height in interval [0; ??]
-		let heightFromDistanceFromCenter = (maxDistance - distanceFromCenter) * normFactor;
+		// Height in interval [0; 48]
+		let heightFromDistanceFromCenter = -16;
+		
+		if (distanceFromCenter <= 128)
+			heightFromDistanceFromCenter = (143021 * Math.pow(distanceFromCenter, 3))/2246553600
+			 - (8545943 * Math.pow(distanceFromCenter, 2))/748851200
+			 - (5229361 * distanceFromCenter)/70204800
+			 + 48;
 
 		let noise = 0;
-		for(let i = 5; i <= 8; i++) {
+		for(let i = 4; i <= 8; i++) {
 			let waveLength = i * 8;
 			noise += this.simplex.noise(x / waveLength, z / waveLength);
 		}
-		// Scale noise from [-1; 1] to [0; 18]
+		// Scale noise from [-1; 1]*4 to [0; 20]*4
 		noise += 1;
-		noise *= 8;
+		noise *= 10;
 
 		// Add noise to heightFromDistanceFromCenter
 		let height = heightFromDistanceFromCenter + noise;
 
+		if(height < 0)
+			height = 0;
+
 		return height;
+	}
+
+	getSandHeightFromSimplex(x, z) {
+		let noise = 0;
+		for(let i = 5; i <= 8; i++) {
+			let waveLength = i * 6;
+			noise += this.simplex.noise(x / waveLength, z / waveLength);
+		}
+		// Scale noise
+		noise += 1;
+		noise *= 3;
+		noise += this.WATER_SURFACE_HEIGHT;
+
+		return noise;
 	}
 
 	getCubeType(coords) {
