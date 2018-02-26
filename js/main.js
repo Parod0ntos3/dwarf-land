8f55fad554d60712b3374c0a5d0d98db99477c99
Tobias Küttner <t.kuettner@ee-mobility.com>
2018-02-26 20:04:29 +0100

    Refactoring of main file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Initialize threejs-variables
 var renderer = new THREE.WebGLRenderer({canvas: document.getElementById("myCanvas"),
 										antialias: true} );
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -9,21 +10,6 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 var controls = new THREE.OrbitControls( camera, renderer.domElement );
 var raycaster = new THREE.Raycaster();
-var mouse = new THREE.Vector2(), INTERSECTED;
-document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-
-var stats;
-var container = document.body;
-stats = new Stats();
-container.appendChild( stats.dom );
-
-var ambientLight = new THREE.AmbientLight("rgb(255, 255, 255)", 0.3);
-scene.add(ambientLight);
-
-var directionalLight = new THREE.DirectionalLight("rgb(255, 255, 255)", 0.8);
-directionalLight.position.set( 1, 1.75, 1 );
-directionalLight.position.multiplyScalar( 30 );
-scene.add(directionalLight);
 
 camera.position.x = 0;
 camera.position.y = 0;
@@ -32,40 +18,35 @@ controls.target.x = 128;
 controls.target.y = 32;
 controls.target.z = 128;
 
-var geometry = new THREE.BoxGeometry( 1.01, 1.01, 1.01 );
-var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// Initialize mouse
+var mouse = new THREE.Vector2(), INTERSECTED;
+document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+function onDocumentMouseMove( event ) {
+	event.preventDefault();
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
 
-var animate = function () {
-	if(textureLoaded === true) {
-		chunkManager = new ChunkManager(chunkManagerTexture, scene, worldData);
-		chunkManagerInitialized = true;
-		textureLoaded = false;
-	}
+// Initialize variables for fps-stats
+var stats;
+var container = document.body;
+stats = new Stats();
+container.appendChild( stats.dom );
 
-	if (chunkManagerInitialized === true) {
-		stats.begin();
+// Initialize lights
+var ambientLight = new THREE.AmbientLight("rgb(255, 255, 255)", 0.3);
+scene.add(ambientLight);
 
-		controls.update();
-		raycaster.setFromCamera( mouse, camera );
-		
-		let mouseIntersectionCoords = worldData.getIntersectionWithMouseRay(raycaster.ray);
+var directionalLight = new THREE.DirectionalLight("rgb(255, 255, 255)", 0.8);
+directionalLight.position.set( 1, 1.75, 1 );
+directionalLight.position.multiplyScalar( 30 );
+scene.add(directionalLight);
 
-		cube.position.x = mouseIntersectionCoords[0];
-		cube.position.y = mouseIntersectionCoords[1];
-		cube.position.z = mouseIntersectionCoords[2];
-
-		renderer.render(scene, camera);
-		stats.end();
-	}
-
-	requestAnimationFrame( animate );
-};
-
+// Initialize worldData and chunkManager
 var worldData = new WorldData();
-
 var worldSideLength = worldData.chunkSize.x * worldData.numberOfChunks.x;
+var chunkManager;
+var chunkManagerInitialized = false;
 
 // Add water-plane to scene
 var waterPlaneMaterial = new THREE.MeshLambertMaterial( {color: "rgb(100,100,150)", side: THREE.DoubleSide} );
@@ -100,13 +81,37 @@ textureLoader.load('./res/cubes.png', function (texture){
 	console.error('texture not loaded', err)
 });
 
-var chunkManager;
-var chunkManagerInitialized = false;
+// Initialize cube which shows the selected cube
+var selechtedCubeGeometry = new THREE.BoxGeometry( 1.01, 1.01, 1.01 );
+var selechtedCubeMaterial = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+var selectedCube = new THREE.Mesh( selechtedCubeGeometry, selechtedCubeMaterial );
+scene.add( selectedCube );
 
-animate();
+// Main game loop
+var main = function () {
+	if(textureLoaded === true) {
+		chunkManager = new ChunkManager(chunkManagerTexture, scene, worldData);
+		chunkManagerInitialized = true;
+		textureLoaded = false;
+	}
 
-function onDocumentMouseMove( event ) {
-	event.preventDefault();
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-}
+	if (chunkManagerInitialized === true) {
+		stats.begin();
+
+		controls.update();
+		raycaster.setFromCamera( mouse, camera );
+		
+		let mouseIntersectionCoords = worldData.getIntersectionWithMouseRay(raycaster.ray);
+
+		selectedCube.position.x = mouseIntersectionCoords[0];
+		selectedCube.position.y = mouseIntersectionCoords[1];
+		selectedCube.position.z = mouseIntersectionCoords[2];
+
+		renderer.render(scene, camera);
+		stats.end();
+	}
+	requestAnimationFrame( main );
+};
+
+// Start game
+main();
