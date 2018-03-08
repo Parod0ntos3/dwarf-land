c4ef935450a640126122b955ae2c0ff7c141edab
Tobias Küttner <t.kuettner@ee-mobility.com>
2018-03-08 07:43:05 +0100

    starting with mining job

--- js/entity/DwarfManager.js
+++ js/entity/DwarfManager.js
@@ -10,6 +10,16 @@ class DwarfManager {
 	update(mousePicker) {
 		this.miningSelection.update(mousePicker);
 
+		// concept:
+		// If miningSelection !== null
+		//	-> look if selection is reachable
+		//	-> give dwarf job mining
+		//	-> mining job pushes walking state to the top (walking to the target)
+
+		if(this.miningSelection.getSelectedCoords().length > 0) {
+			let reachableSelection = this.getReachableSelectedCoords(this.miningSelection.getSelectedCoords());	
+		}
+
 		if(mouse.rightClicked === true && mousePicker.getSelectedCubeCoords() !== undefined) {
 			let clickedCoords = [mousePicker.getSelectedCubeCoords()[0],
 								 mousePicker.getSelectedCubeCoords()[1] + 1,
@@ -21,5 +31,54 @@ class DwarfManager {
 		}
 
 		this.dwarf.update();
+	}
+
+	getReachableSelectedCoords(selectedCoords) {
+		iLoop: for(let i = 0; i < selectedCoords.length; i++) {
+
+			let neighborsTypes = this.worldData.getNeighborsTypes(selectedCoords[i]);
+			let isNeighborSolidArray = [];
+			let allNeighborsAreSolid = true;
+			for(let j = 0; j < 6; j++) {
+				// Check if neighbor cube is solid
+				if( neighborsTypes[j] !== this.worldData.CUBE_TYPE_AIR && 
+					neighborsTypes[j] !== this.worldData.CUBE_TYPE_WATER) {
+				   	isNeighborSolidArray.push(true);
+				} else {
+				   	isNeighborSolidArray.push(false);
+				   	allNeighborsAreSolid = false;
+				}
+			}
+
+			if(allNeighborsAreSolid === true) {
+				continue iLoop;
+			}
+
+			// Cases:
+			// Standing next to the cube:
+			//		- standing one layer up
+			//		- on the same layer
+			//		- one layer down
+			//		- standing directly under the cube
+
+			let walkableNeighborsWhichReachCube = [];
+			for(let x = -1; x <= 1; x++) {
+				for(let z = -1; z <= 1; z++) {
+					for(let y = -1; y <= 2; y++) {
+						let coords = [
+							selectedCoords[i][0] + x,
+							selectedCoords[i][1] + y,
+							selectedCoords[i][2] + z
+						]
+						if(this.worldData.getCubeWalkability(coords) === 1) {
+							walkableNeighborsWhichReachCube.push(coords);
+						}
+						. . .
+						. . .
+						. . .
+					}
+				}				
+			}
+		}
 	}
 }
